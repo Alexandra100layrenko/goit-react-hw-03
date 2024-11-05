565c9346e073cc3d61ba6fc107163b76450d0891
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2024-11-05 17:57:46 +0000

    update all

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Formik } from 'formik';
+import './App.module.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Form</h1>
+    </>
+  )
+}
+
+export default App
