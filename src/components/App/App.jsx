5ce9ba217290b52c6164a8d82293d186cb423642
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2024-11-14 18:04:32 +0000

    initiall ContactForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
-import { Formik } from 'formik';
+import { nanoid } from 'nanoid'
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import ContactForm from '../ContactForm/ContactForm'
 import SearchBox from '../SearchBox/SearchBox'
 import ContactList from '../ContactList/ContactList'
@@ -19,11 +20,21 @@ function App() {
     ]
   });
 
-  const [ filter, setFilter ] = useState('');
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
+
+  const addContact = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
 
   const deleteContact = (contactId) => {
     setContacts(contacts.filter(contact => contact.id !== contactId));
@@ -40,7 +51,7 @@ function App() {
   return (
     <div className={s.wraper}>
       <h1>Phonebook</h1>
-      <ContactForm />
+      <ContactForm addContact={addContact} />
       <SearchBox value={filter} onChange={handleSearchChange} />
       <ContactList value={contacts} contacts={filteredContacts} deleteContact={deleteContact} />
     </div>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,49 @@
+import * as Yup from 'yup'
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import s from './ContactForm.module.css'
 
-const ContactForm = () => {
+const ContactForm = ({ addContact }) => {
+    
+    const handleSubmit = (values, options) => {
+        addContact(values);
+        options.resetForm();
+    }
+    
+    const initialValues = {
+        name: '',
+        number: '',
+      };
+
+    const orderSchema = Yup.object().shape({
+        name: Yup.string()
+            .min(3, 'Name must be at least 3 characters')
+            .max(50, 'Name cannot exceed 50 characters')
+            .required('Name is required'),
+        number: Yup.string()
+            .matches(/^\+?[0-9]{10,14}$/, 'Only numeric characters are allowed')
+            .min(10, 'Minimum 10 characters required')
+            .max(14, 'Maximum 14 characters allowed')
+            .required('Number is required'),
+    })
+
     return(
-        <>
-        
-        </>
+        <div className={s.wraper}>
+            <Formik validationSchema={orderSchema} onSubmit={handleSubmit} initialValues={initialValues} >
+                <Form className={s.form}>
+                    <label className={s.label}>
+                        <span>Name</span>
+                        <Field className={s.input} name='name' />
+                        <ErrorMessage name = 'name' component='span' className={s.errorMessage} />
+                    </label>
+                    <label className={s.label}>
+                        <span>Number</span>
+                        <Field className={s.input} name='number' />
+                        <ErrorMessage name='number' component='span' className={s.errorMessage} />
+                    </label>
+                    <button className={s.btnAdd} type='submit'>Add contact</button>
+                </Form>
+            </Formik>
+        </div>
     )
 }
 
